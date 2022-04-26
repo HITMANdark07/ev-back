3cb2e9b4681f5485cccf37e3009661763fd7bf3a
HITMANdark07 <shekharkumar235838@gmail.com>
2022-04-27 00:33:50 +0530

    added limit and skip properly

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -4,9 +4,19 @@ const { errorHandler } = require('../helpers/dbErrorHandler');
 exports.listTransactions = async(req, res) => {
     const { limit, skip} = req.query;
     let count = await Order.countDocuments({})
-    let transactions = await Order.find({}).limit(limit).skip(skip).sort({'createdAt':-1});
-    res.status(200).json({
-        total:count,
-        transactions:transactions
+    Order.find({})
+    .sort({"createdAt":-1})
+    .limit(parseInt(limit))
+    .skip(parseInt(skip))
+    .exec((err, orders) => {
+        if(err || !orders){
+            return res.status(400).json({
+                message: errorHandler(err)
+            })
+        }
+        res.status(200).json({
+            total:count,
+            transactions:orders
+        })
     })
 }
