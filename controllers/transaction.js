682c3b16644db89a423bb97efa3eea47f110e07f
HITMANdark07 <shekharkumar235838@gmail.com>
2022-04-27 00:48:37 +0530

    transaction ppopluated with data

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -5,6 +5,7 @@ exports.listTransactions = async(req, res) => {
     const { limit, skip} = req.query;
     let count = await Order.countDocuments({})
     Order.find({})
+    .populate("user","name email phone")
     .sort({"createdAt":-1})
     .limit(parseInt(limit))
     .skip(parseInt(skip))
