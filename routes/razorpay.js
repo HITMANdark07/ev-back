8777b1f6fd24d8bc5b5430f3cceb896c796e76c6
shekhar kumar <shekhar.kumar@investwithtribe.com>
2022-12-16 03:53:39 +0530

    verify fix

--- routes/razorpay.js
+++ routes/razorpay.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-const { verify } = require('jsonwebtoken');
 const { createOrder, findOrderById, read, updateOrder,verify } = require('../controllers/razorpay');
 
 router.get("/:orderId",read);
