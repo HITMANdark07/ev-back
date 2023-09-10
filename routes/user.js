37b93352e283342f8ff43d69f21bc74a47dbb7eb
Shekhar <shekharkumar235838@gmail.com>
2023-09-10 20:50:01 +0530

    get partner profiles

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const { login, list, search, setOtp, verifyOtp, getProfile, userById } = require('../controllers/user');
+const {
+  login,
+  list,
+  search,
+  setOtp,
+  verifyOtp,
+  getProfile,
+  userById,
+  getPartnersProfile,
+} = require("../controllers/user");
 
 router.post("/login", login);
-router.get("/user/get-profile/:userId",getProfile);
-router.get("/user/list",list);
-router.get("/user/search/list",search);
+router.get("/user/get-profile/:userId", getProfile);
+router.get("/user/list", list);
+router.get("/user/search/list", search);
 router.post("/user/send-otp", setOtp);
 router.post("/user/verify-otp", verifyOtp);
+router.get("/get-partner-profiles", getPartnersProfile);
 
-router.param("userId",userById);
+router.param("userId", userById);
 
 module.exports = router;
